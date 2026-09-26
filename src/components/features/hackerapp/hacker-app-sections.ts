772975cec5d0e418d8207d0bd234799919cf6872
Shannon <shannon.stefanus@gmail.com>
2026-09-25 18:29:11 -0700

    fix(hackerapp): gate basic info question types

--- src/components/features/hackerapp/hacker-app-sections.ts
+++ src/components/features/hackerapp/hacker-app-sections.ts
@@ -74,6 +74,10 @@ export const isFormInputAllowedInSection = (
 export const QUESTION_TYPE_SECTION: Partial<
   Record<HackerApplicationQuestionType, HackerApplicationSections>
 > = {
+  "Full Legal Name": "BasicInfo",
+  School: "BasicInfo",
+  Major: "BasicInfo",
+  Country: "BasicInfo",
   Portfolio: "Skills",
   Github: "Skills",
   LinkedIn: "Skills",
